8ca130e0907e62c50a48d976611bdae430e4553e
Kuba <gpro6833@gmail.com>
2023-02-27 17:22:39 +0100

    fixed

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ const next_img = document.querySelector('.next_img');
 const previous_img = document.querySelector('.previous_img');
 const img_down = document.querySelectorAll('.img_down');
 let currentImgIndex;
+img_down.forEach((el) => {
+	el.addEventListener('click', (e) => {
+		jump.classList.remove('jump_hidden');
+		jump_img.src = e.target.src;
+	});
+});
 photo.forEach((el, i) => {
-	
-
-
 	el.addEventListener('click', (e) => {
 		jump.classList.remove('jump_hidden');
 		jump_img.src = e.target.src;
