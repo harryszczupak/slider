dd4d01d186b857185089c022b4331fcb47805efd
Kuba <gpro6833@gmail.com>
2023-02-27 18:32:48 +0100

    active border left

--- index.js
+++ index.js
@@ -4,19 +4,24 @@ const jump_img = document.querySelector('.jump_img');
 const jump_cancel = document.querySelector('.jump_cancel');
 const next_img = document.querySelector('.next_img');
 const previous_img = document.querySelector('.previous_img');
-const img_down = document.querySelectorAll('.img_down');
+const img_down = document.querySelectorAll('.img_downAll');
+const topDescription = document.querySelector('span');
 let currentImgIndex;
+
 img_down.forEach((el) => {
 	el.addEventListener('click', (e) => {
 		jump.classList.remove('jump_hidden');
 		jump_img.src = e.target.src;
 	});
 });
+let points = 0;
 photo.forEach((el, i) => {
+	points++;
 	el.addEventListener('click', (e) => {
 		jump.classList.remove('jump_hidden');
 		jump_img.src = e.target.src;
 		currentImgIndex = i;
+		topDescription.innerHTML = currentImgIndex + 1 + '/' + points;
 	});
 	jump_cancel.addEventListener('click', () => {
 		jump.classList.add('jump_hidden');
@@ -29,6 +34,7 @@ next_img.addEventListener('click', () => {
 		currentImgIndex++;
 	}
 	jump_img.src = photo[currentImgIndex].src;
+	topDescription.innerHTML = currentImgIndex + 1 + '/' + points;
 });
 previous_img.addEventListener('click', () => {
 	if (currentImgIndex === 0) {
@@ -37,4 +43,5 @@ previous_img.addEventListener('click', () => {
 		currentImgIndex--;
 	}
 	jump_img.src = photo[currentImgIndex].src;
+	topDescription.innerHTML = currentImgIndex + 1 + '/' + points;
 });
