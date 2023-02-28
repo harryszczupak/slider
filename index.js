ca65d9cdf1b47bafafb0233214b3768aa36b6b6d
Kuba <gpro6833@gmail.com>
2023-02-28 17:26:53 +0100

    added bottom desc

--- index.js
+++ index.js
@@ -6,15 +6,29 @@ const next_img = document.querySelector('.next_img');
 const previous_img = document.querySelector('.previous_img');
 const img_down = document.querySelectorAll('.img_downAll');
 const topDescription = document.querySelector('span');
+const bottomDescription = document.querySelector('.description');
 let currentImgIndex;
-
-img_down.forEach((el) => {
-	el.addEventListener('click', (e) => {
-		jump.classList.remove('jump_hidden');
-		jump_img.src = e.target.src;
-	});
-});
 let points = 0;
+function arrow_description() {
+	if (currentImgIndex == 0) {
+		bottomDescription.innerHTML = 'lake';
+	} else if (currentImgIndex == 1) {
+		bottomDescription.innerHTML = 'the Chinese wall';
+	} else {
+		bottomDescription.innerHTML = 'Misisipi';
+	}
+}
+
+function ChangeBottom_img() {
+	img_down.forEach((el) => {
+		el.addEventListener('click', (e) => {
+			jump.classList.remove('jump_hidden');
+			jump_img.src = e.target.src;
+		});
+	});
+}
+ChangeBottom_img();
+
 photo.forEach((el, i) => {
 	points++;
 	el.addEventListener('click', (e) => {
@@ -22,26 +36,50 @@ photo.forEach((el, i) => {
 		jump_img.src = e.target.src;
 		currentImgIndex = i;
 		topDescription.innerHTML = currentImgIndex + 1 + '/' + points;
+
+		function main_Bottomdescription() {
+			if (el == photo[0]) {
+				bottomDescription.innerHTML = 'lake';
+			} else if (el == photo[1]) {
+				bottomDescription.innerHTML = 'the Chinese wall';
+			} else {
+				bottomDescription.innerHTML = 'Misisipi';
+			}
+		}
+		main_Bottomdescription();
 	});
-	jump_cancel.addEventListener('click', () => {
-		jump.classList.add('jump_hidden');
+	function exitButton() {
+		jump_cancel.addEventListener('click', () => {
+			jump.classList.add('jump_hidden');
+		});
+	}
+	exitButton();
+});
+function rightArrow() {
+	next_img.addEventListener('click', () => {
+		if (currentImgIndex === photo.length - 1) {
+			currentImgIndex = 0;
+		} else {
+			currentImgIndex++;
+		}
+		jump_img.src = photo[currentImgIndex].src;
+		topDescription.innerHTML = currentImgIndex + 1 + '/' + points;
+
+		arrow_description();
 	});
-});
-next_img.addEventListener('click', () => {
-	if (currentImgIndex === photo.length - 1) {
-		currentImgIndex = 0;
-	} else {
-		currentImgIndex++;
-	}
-	jump_img.src = photo[currentImgIndex].src;
-	topDescription.innerHTML = currentImgIndex + 1 + '/' + points;
-});
-previous_img.addEventListener('click', () => {
-	if (currentImgIndex === 0) {
-		currentImgIndex = photo.length - 1;
-	} else {
-		currentImgIndex--;
-	}
-	jump_img.src = photo[currentImgIndex].src;
-	topDescription.innerHTML = currentImgIndex + 1 + '/' + points;
-});
+}
+rightArrow();
+function leftArrow() {
+	previous_img.addEventListener('click', () => {
+		if (currentImgIndex === 0) {
+			currentImgIndex = photo.length - 1;
+		} else {
+			currentImgIndex--;
+		}
+		jump_img.src = photo[currentImgIndex].src;
+		topDescription.innerHTML = currentImgIndex + 1 + '/' + points;
+
+		arrow_description();
+	});
+}
+leftArrow();
